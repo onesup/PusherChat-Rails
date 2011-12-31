b3698a40d907b1a5554a86c261dd22d5ae559ab7
Sickboy <sb@dev-heaven.net>
2011-12-31 17:38:07 +0100

    Prepared Supposed IE fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
-
+  # Add IE support to accept cookies when using pusher chat inside an iframe
+  #before_filter { response.headers['P3P'] = %q|CP="HONK"| }
 end
